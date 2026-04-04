ca248c5e5900a57c574e4dc8a252606d95e01438
vizay2 <vizay.yadav.2@gmail.com>
2026-04-05 00:42:12 +0530

    Update DBMSLAB6.sql

--- assignment06/DBMSLAB6.sql
+++ assignment06/DBMSLAB6.sql
@@ -75,96 +75,38 @@ SQL> SELECT CourseID, COUNT(StudentID) AS Enrolled_Students
    JOIN Enrollment e ON c.CourseID = e.CourseID
    GROUP BY c.CourseName;
 
-COURSENAME                                                                                           ENROLLED_STUDENTS
----------------------------------------------------------------------------------------------------- -----------------
-ELECTROMAGNETIC THEORY                                                                                               1
-INDUSTRIAL ELECTRICAL SYSTEMS                                                                                        1
-HYDRAULIC ENGINEERING                                                                                                1
-COMPUTER NETWORK            
+-------------- QUESTION 17 --------------
+ SELECT DepartmentID, COUNT(*) AS Total_Students
+  FROM Student
+  GROUP BY DepartmentID;
 
-SQL> SELECT DepartmentID, COUNT(*) AS Total_Students
-  2  FROM Student
-  3  GROUP BY DepartmentID;
+---------------QUESTION 18 --------------
+SELECT f.FacultyID, COUNT(c.CourseID) AS Courses_Taught
+   FROM Faculty f
+   JOIN Course c ON f.FacultyID = c.FacultyID
+   GROUP BY f.FacultyID;
 
-DEPARTMENT TOTAL_STUDENTS
----------- --------------
-105                     2
-157                     3
-101                     1
-103                     1
-119                     1
-118                     1
-153                     1
+------------------------------------------ PART E------------------------------------------
+--------------QUESTION 19--------------
+SELECT c.CourseName, MAX(e.Grade) AS Max_Grade
+  FROM Course c
+  JOIN Enrollment e ON c.CourseID = e.CourseID
+  GROUP BY c.CourseName;
 
-7 rows selected.
+-------------- QUESTION 20--------------
+ SELECT DepartmentID, COUNT(*) AS Total_Courses
+    FROM Course
+  GROUP BY DepartmentID;
 
-SQL> SELECT f.FacultyID, COUNT(c.CourseID) AS Courses_Taught
-  2  FROM Faculty f
-  3  JOIN Course c ON f.FacultyID = c.FacultyID
-  4  GROUP BY f.FacultyID;
+-------------- QUESTION 21--------------
+SELECT Semester, COUNT(StudentID) AS Total_Students
+   FROM Enrollment
+   GROUP BY Semester;
 
-FACULTYID  COURSES_TAUGHT
----------- --------------
-1004                    2
-1006                    1
-1002                    1
-1001                    1
-1007                    1
-1010                    2
-1003                    1
-1008                    1
+-------------- QUESTION 22--------------
+ SELECT c.CourseName, COUNT(e.StudentID) AS Total_Students
+   FROM Course c
+   JOIN Enrollment e ON c.CourseID = e.CourseID
+   GROUP BY c.CourseName
+   HAVING COUNT(e.StudentID) > 2;
 
-8 rows selected.
-
-SQL> SELECT c.CourseName, MAX(e.Grade) AS Max_Grade
-  2  FROM Course c
-  3  JOIN Enrollment e ON c.CourseID = e.CourseID
-  4  GROUP BY c.CourseName;
-
-COURSENAME                                                                                           MA
----------------------------------------------------------------------------------------------------- --
-MACHINE LEARNING                                                                                     A
-COMPILER DESIGN                                                                                      A+
-ELECTROMAGNETIC THEORY                                                                               A
-HYDRAULIC ENGINEERING                                                                                A-
-OPERATING SYSTEMS                                                                                    B+
-INDUSTRIAL ELECTRICAL SYSTEMS                                                                        A
-COMPUTER NETWORK                                                                                     A+
-
-7 rows selected.
-
-SQL> SELECT DepartmentID, COUNT(*) AS Total_Courses
-  2  FROM Course
-  3  GROUP BY DepartmentID;
-
-DEPARTMENT TOTAL_COURSES
----------- -------------
-157                    4
-101                    2
-103                    2
-118                    1
-102                    1
-
-SQL> SELECT Semester, COUNT(StudentID) AS Total_Students
-  2  FROM Enrollment
-  3  GROUP BY Semester;
-
-SEMESTER             TOTAL_STUDENTS
--------------------- --------------
-Semester 3                        5
-Semester 4                        4
-Semester 5                        1
-
-SQL> SELECT c.CourseName, COUNT(e.StudentID) AS Total_Students
-  2  FROM Course c
-  3  JOIN Enrollment e ON c.CourseID = e.CourseID
-  4  GROUP BY c.CourseName
-  5  HAVING COUNT(e.StudentID) > 2;
-
-COURSENAME                                                                                           TOTAL_STUDENTS
----------------------------------------------------------------------------------------------------- --------------
-COMPUTER NETWORK                                                                                                  3
-
-SQL> SAVE DBMSDBMSLAB6.sql
-Created file DBMSDBMSLAB6.sql
-SQL>
