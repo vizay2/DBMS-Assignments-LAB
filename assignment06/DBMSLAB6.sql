02a2e11e938fee0d44af5247ec1632b5e8f051a8
vizay2 <vizay.yadav.2@gmail.com>
2026-04-05 00:35:09 +0530

    Update DBMSLAB6.sql

--- assignment06/DBMSLAB6.sql
+++ assignment06/DBMSLAB6.sql
@@ -1,272 +1,86 @@
+------------------------------------------PART A------------------------------------------
+--------------QUESTION 01--------------
 SELECT COUNT(*) AS Total_Students
-  2    FROM Student;
+   FROM Student;
+--------------QUESTION 02--------------
 
-TOTAL_STUDENTS
---------------
-            10
+ SELECT COUNT(*) AS Total_Faculty
+      FROM Faculty;
+--------------QUESTION 03--------------
+SELECT COUNT(*) AS Total_Courses_Available
+   FROM Course;
+--------------QUESTION 04--------------
+ SELECT MAX(Credits) AS Max_Credits
+    FROM Course;
+--------------QUESTION 05--------------
+ SELECT MIN(Credits) AS Min_Credits
+   FROM Course;
+------------------------------------------PART B------------------------------------------
+--------------QUESTION 06--------------
 
-SQL> SELECT COUNT(*) AS Total_Faculty
-  2    FROM Faculty;
+ SELECT DepartmentID,COUNT(*) AS No_Of_Students
+  FROM Student
+   GROUP BY DepartmentID;
+--------------QUESTION 07--------------
 
-TOTAL_FACULTY
--------------
-           10
+SELECT DepartmentID, COUNT(*) AS No_Of_Faculty
+   FROM Faculty
+    GROUP BY DepartmentID;
+--------------QUESTION 08--------------
+ SELECT DepartmentID, COUNT(*) AS No_Of_Courses
+   FROM Course
+   GROUP BY DepartmentID;
+--------------QUESTION 09--------------
+ SELECT Semester, COUNT(*) AS Total_Enrollments
+    FROM Enrollment
+    GROUP BY Semester;
+--------------QUESTION 10--------------
+ SELECT Semester, COUNT(*) AS Total_Enrollments
+   FROM Enrollment
+   GROUP BY Grade;
+------------------------------------------ PART C------------------------------------------
+--------------QUESTION 11--------------
+ SELECT Grade, COUNT(*) AS Total_Enrollments
+    FROM Enrollment
+    GROUP BY Grade;
+--------------QUESTION 12--------------
+ SELECT DepartmentID, COUNT(*) AS Total_Students
+   FROM Student
+   GROUP BY DepartmentID
+  HAVING COUNT(*) > 3;
+--------------QUESTION 13--------------
+ SELECT Semester, COUNT(*) AS Total_Enrollments
+ FROM Enrollment
+ GROUP BY Semester
+ HAVING COUNT(*) > 2;
+--------------QUESTION 14--------------
 
-SQL> SELECT COUNT(*) AS Total_Courses_Available
-  2    FROM Course;
-
-TOTAL_COURSES_AVAILABLE
------------------------
-                     10
-
-SQL> SELECT MAX(Credits) AS Max_Credits
-  2    FROM Course;
-
-MAX_CREDITS
------------
-          4
-
-SQL> SELECT MIN(Credits) AS Min_Credits
-  2  FROM Course;
-
-MIN_CREDITS
------------
-          3
-
-SQL> SELECT Department_ID,COUNT(*) AS No_Of_Students
-  2    FROM Student
-  3    GROUP BY Department_ID;
-  GROUP BY Department_ID
-           *
-ERROR at line 3:
-ORA-00904: "DEPARTMENT_ID": invalid identifier
-
-
-SQL> SELECT DepartmentID,COUNT(*) AS No_Of_Students
-  2    FROM Student
-  3    GROUP BY DepartmentID;
-
-DEPARTMENT NO_OF_STUDENTS
----------- --------------
-105                     2
-157                     3
-101                     1
-103                     1
-119                     1
-118                     1
-153                     1
-
-7 rows selected.
-
-SQL> SELECT DepartmentID, COUNT(*) AS No_Of_Faculty
-  2  FROM Faculty
-  3  GROUP BY DepartmentID;
-
-DEPARTMENT NO_OF_FACULTY
----------- -------------
-105                    1
-157                    2
-118                    1
-153                    1
-101                    2
-119                    1
-102                    1
-103                    1
-
-8 rows selected.
-
-SQL> SELECT DepartmentID, COUNT(*) AS No_Of_Courses
-  2  FROM Course
-  3  GROUP BY DepartmentID;
-
-DEPARTMENT NO_OF_COURSES
----------- -------------
-157                    4
-101                    2
-103                    2
-118                    1
-102                    1
-
-SQL> SELECT Semester, COUNT(*) AS Total_Enrollments
-  2  FROM Enrollment
-  3  GROUP BY Semester;
-
-SEMESTER             TOTAL_ENROLLMENTS
--------------------- -----------------
-Semester 3                           5
-Semester 4                           4
-Semester 5                           1
-
-SQL> SELECT Semester, COUNT(*) AS Total_Enrollments
-  2  FROM Enrollment
-  3  GROUP BY Grade;
-SELECT Semester, COUNT(*) AS Total_Enrollments
-       *
-ERROR at line 1:
-ORA-00979: not a GROUP BY expression
-
-
-SQL> SELECT Grade, COUNT(*) AS Total_Enrollments
-  2  FROM Enrollment
-  3  GROUP BY Grade;
-
-GR TOTAL_ENROLLMENTS
--- -----------------
-A                  6
-A+                 2
-A-                 1
-B+                 1
-
-SQL> SELECT DepartmentID, COUNT(*) AS Total_Students
-  2  FROM Student
-  3  GROUP BY DepartmentID
-  4  HAVING COUNT(*) > 3;
-
-no rows selected
-
-SQL> SELECT Semester, COUNT(*) AS Total_Enrollments
-  2  FROM Enrollment
-  3  GROUP BY Semester
-  4  HAVING COUNT(*) > 2;
-
-SEMESTER             TOTAL_ENROLLMENTS
--------------------- -----------------
-Semester 3                           5
-Semester 4                           4
-
-SQL> SELECT Grade, COUNT(*) AS Count_Grade
-  2  FROM Enrollment
-  3  GROUP BY Grade
-  4  HAVING COUNT(*) > 1;
-
-GR COUNT_GRADE
--- -----------
-A            6
-A+           2
-
-SQL> SELECT Department_ID, COUNT(*) AS Total_Courses
-  2  FROM Course
-  3  GROUP BY Department_ID
-  4
-SQL> SELECT DepartmentID, COUNT(*) AS Total_Courses
-  2  FROM Course
-  3  GROUP BY DepartmentID
-  4  HAVING COUNT(*) > 1;
-
-DEPARTMENT TOTAL_COURSES
----------- -------------
-157                    4
-101                    2
-103                    2
-
+ SELECT Grade, COUNT(*) AS Count_Grade
+   FROM Enrollment
+   GROUP BY Grade
+   HAVING COUNT(*) > 1;
+------------------------------------------ PART D------------------------------------------
+--------------QUESTION 15--------------
+ SELECT DepartmentID, COUNT(*) AS Total_Courses
+   FROM Course
+    GROUP BY DepartmentID
+    HAVING COUNT(*) > 1;
+-------------- QUESTION 16--------------
 SQL> SELECT CourseID, COUNT(StudentID) AS Enrolled_Students
   2  FROM Enrollment
   3  GROUP BY CourseID;
-
-COURSEID   ENROLLED_STUDENTS
----------- -----------------
-103404                     1
-103606                     1
-101504                     1
-157503                     3
-157601                     2
-157504                     1
-105403                     1
-
-7 rows selected.
-
-SQL> SELECT c.CourseName, COUNT(e.Student_D) AS Enrolled_Students
-  2  FROM Course c
-  3  JOIN Enrollment e ON c.CourseID = e.CourseID
-  4  GROUP BY c.CourseName;
-SELECT c.CourseName, COUNT(e.Student_D) AS Enrolled_Students
-                           *
-ERROR at line 1:
-ORA-00904: "E"."STUDENT_D": invalid identifier
-
-
-SQL> SELECT c.CourseName, COUNT(e.StudentD) AS Enrolled_Students
-  2  FROM Course c
-  3  JOIN Enrollment e ON c.CourseID = e.CourseID
-  4  GROUP BY c.CourseName;
-SELECT c.CourseName, COUNT(e.StudentD) AS Enrolled_Students
-                           *
-ERROR at line 1:
-ORA-00904: "E"."STUDENTD": invalid identifier
-
-
-SQL> SELECT c.CourseName, COUNT(e.StudentID) AS Enrolled_Students
-  2  FROM Course c
-  3  JOIN Enrollment e ON c.CourseID = e.CourseID
-  4  GROUP BY c.CourseName;
-
-COURSENAME
---------------------------------------------------------------------------------
-ENROLLED_STUDENTS
------------------
-ELECTROMAGNETIC THEORY
-                1
-
-INDUSTRIAL ELECTRICAL SYSTEMS
-                1
-
-HYDRAULIC ENGINEERING
-                1
-
-
-COURSENAME
---------------------------------------------------------------------------------
-ENROLLED_STUDENTS
------------------
-COMPUTER NETWORK
-                3
-
-COMPILER DESIGN
-                2
-
-MACHINE LEARNING
-                1
-
-
-COURSENAME
---------------------------------------------------------------------------------
-ENROLLED_STUDENTS
------------------
-OPERATING SYSTEMS
-                1
-
-
-7 rows selected.
-
-SQL> SET LINESIZE 200
-SQL> SET PAGESIZE 100
-SQL> SET WRAP OFFSELECT c.CourseName, COUNT(e.StudentID) AS Enrolled_Students
-SP2-0265: wrap must be set ON or OFF
-SQL> FROM Course c
-SP2-0734: unknown command beginning "FROM Cours..." - rest of line ignored.
-SQL> JOIN Enrollment e ON c.CourseID = e.CourseID
-SP2-0734: unknown command beginning "JOIN Enrol..." - rest of line ignored.
-SQL> GROUP BY c.Course_Name;
-SP2-0734: unknown command beginning "GROUP BY c..." - rest of line ignored.
-SQL> SET LINESIZE 200
-SQL> SET PAGESIZE 100
-SQL> SET WRAP OFF
-SQL> SELECT c.CourseName, COUNT(e.StudentID) AS Enrolled_Students
-  2  FROM Course c
-  3  JOIN Enrollment e ON c.CourseID = e.CourseID
-  4  GROUP BY c.CourseName;
+-------------- QUESTION 16--------------
+ SELECT c.CourseName, COUNT(e.StudentID) AS Enrolled_Students
+   FROM Course c
+   JOIN Enrollment e ON c.CourseID = e.CourseID
+   GROUP BY c.CourseName;
 
 COURSENAME                                                                                           ENROLLED_STUDENTS
 ---------------------------------------------------------------------------------------------------- -----------------
 ELECTROMAGNETIC THEORY                                                                                               1
 INDUSTRIAL ELECTRICAL SYSTEMS                                                                                        1
 HYDRAULIC ENGINEERING                                                                                                1
-COMPUTER NETWORK                                                                                                     3
-COMPILER DESIGN                                                                                                      2
-MACHINE LEARNING                                                                                                     1
-OPERATING SYSTEMS                                                                                                    1
-
-7 rows selected.
+COMPUTER NETWORK            
 
 SQL> SELECT DepartmentID, COUNT(*) AS Total_Students
   2  FROM Student
